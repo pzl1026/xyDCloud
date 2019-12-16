5fa6f1ade9cf008ce7300ffc2012b8d866d87778
Zhiliang <Zhiliang.Peng@handeson.com>
2019-12-16 14:18:11 +0800

    test

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const path = require('path')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+console.log(app, 'app')
+console.log(require.resolve('electron'));
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -18,8 +21,8 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  // mainWindow.loadFile('http://localhost:8000/')
-  mainWindow.loadURL("http://localhost:8000/");
+  // mainWindow.loadFile('./index.html')
+  mainWindow.loadURL("http://localhost:8090/");
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
@@ -31,6 +34,36 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  mainWindow.webContents.session.on('will-download', (event, item, webContents) => {
+    // 设置保存路径,使Electron不提示保存对话框。
+    item.setSavePath('D:\\test\\a.mp4')
+  
+    item.on('updated', (event, state) => {
+      if (state === 'interrupted') {
+        console.log('Download is interrupted but can be resumed');
+        // item.resume()
+      } else if (state === 'progressing') {
+        if (item.isPaused()) {
+          console.log('Download is paused')
+        } else {
+          console.log(`Received bytes: ${item.getReceivedBytes()}`)
+        }
+      }
+    })
+    item.once('done', (event, state) => {
+      console.log(state, 'state')
+      if (state === 'completed') {
+        console.log('Download successfully')
+      } else {
+        console.log(`Download failed: ${state}`)
+      }
+    })
+  });
+
+  setTimeout(function () {
+    mainWindow.webContents.downloadURL('http://video2.uxinyue.com/Act-ss-mp4-ld/a846ee7b59144f61bfabe8cd2b55f534/95d48f76e4dd86780f3bb1d59600451d-ed0fb0984d6e3148afb17af08574000b.mp4');
+  }, 5000)
 }
 
 // This method will be called when Electron has finished
@@ -50,6 +83,8 @@ app.on('activate', function () {
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
+
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
