870c2871ed09313d933817789ef085187c4ebb01
pzl1026 <pzl1026@aliyun.com>
2020-02-13 13:45:08 +0800

    commit

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ function handleMenu() {
   Menu.setApplicationMenu(menu)
 }
 
-
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -32,8 +31,8 @@ function createWindow () {
     height: 800,
     minWidth: 1280,
     minHeight: 800,
-    // maxWidth: 1280,
-    // maxHeight: 800,
+    maxWidth: 1280,
+    maxHeight: 800,
     show: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
@@ -43,11 +42,11 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  // mainWindow.loadFile('./dist/index.html')
-  mainWindow.loadURL("http://localhost:8001");
+  mainWindow.loadFile('./dist/index.html')
+  // mainWindow.loadURL("http://localhost:8001");
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -75,10 +74,14 @@ function handleTray() {
     tray = new Tray(image);
    
     tray.on('click', function (e) {
-      if (!mainWindow.isVisible()) {
-        mainWindow.show();
+      if (!mainWindow) {
+        createWindow();
       } else {
-        mainWindow.hide();
+        if (!mainWindow.isVisible()) {
+          mainWindow.show();
+        } else {
+          mainWindow.hide();
+        }
       }
     });
   }
@@ -91,6 +94,7 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
+  
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit();
@@ -106,7 +110,11 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+  if (mainWindow === null) {
+    createWindow();
+  } else {
+    mainWindow.show();
+  }
 })
 
 
